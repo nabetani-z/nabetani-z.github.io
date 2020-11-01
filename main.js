fc639c7892403791f81002fd6dc23d196a340297
nabetani-z <nabetani.z.gm@gmail.com>
2020-11-01 19:13:19 +0900

    グラフ書く

--- main.js
+++ main.js
@@ -43,28 +43,91 @@ const appendSVG = (p, tag, attrs = {}) => {
   }
   p.appendChild(c);
   return c;
+};
+
+const nowString = () => {
+  let t = new Date();
+  let y = t.getFullYear();
+  let m = t.getMonth() + 1;
+  let d = t.getDate();
+  return `${y}-${m}-${d}`;
+};
+
+const toTick = (s) => {
+  let m = s.match(/(\d+)\D+(\d+)\D+(\d+)/);
+  let yy = parseInt(m[1], 10);
+  let mm = parseInt(m[2], 10);
+  let dd = parseInt(m[3], 10);
+  return (new Date(yy, mm, dd)).getTime();
 }
 
+const rangeOf = (o) => {
+  let dMin = 1e100;
+  let dMax = -1e100;
+  let now = nowString();
+  for (let m of o.members) {
+    dMin = Math.min(dMin, toTick(m.in));
+    dMax = Math.max(dMax, toTick(m.out || o.end || now));
+  }
+  return { min: dMin, max: dMax };
+};
+
+const xpos = (w, range, tText) => {
+  const t = toTick(tText);
+  const d = (t - range.min) / (range.max - range.min);
+  return d * w * 0.8 + w * 0.2;
+};
+
+
 const bulidSVG = (o, e) => {
+  console.log(JSON.stringify(o));
   e.innerHTML = ""
-  let pstyle = `height:${o.height}${o.unit};width:${o.width}${o.unit}`;
+  const pstyle = `height:${o.height}${o.unit};width:${o.width}${o.unit}`;
   e.setAttribute("style", pstyle)
+  let now = nowString();
+  const gap = 3;
+  const h = o.members.length * 10;
+  const w = (h + gap * 2) * o.width / o.height - gap * 2
+  let range = rangeOf(o);
   let svg = appendSVG(e, "svg", {
     xmlns: "http://www.w3.org/2000/svg",
     height: o.height + o.unit,
     width: o.width + o.unit,
-    viewBox: "-1 -1 38 38"
+    viewBox: `${-gap} ${-gap} ${w + gap * 2} ${h + gap * 2}`
   });
-  appendSVG(svg, "circle", { cx: 50, cy: 50, r: 40, stroke: "black", "stroke-width": 3, fill: "red" });
-}
+  let graphs = appendSVG(svg, "g", { fill: "#eee" });
+  for (let i = 0; i < o.members.length; ++i) {
+    let m = o.members[i];
+    let col = m.color || "rgb(0,0,255)";
+    let start = xpos(w, range, m.in);
+    let end = xpos(w, range, m.out || o.end || now);
+    const barH = 3;
+    appendSVG(graphs, "rect", {
+      height: barH,
+      width: end - start,
+      x: start,
+      y: i * 10 + (10 - barH) / 2,
+      style: `fill: ${col}`,
+    });
+  }
+  let texts = appendSVG(svg, "g", {
+    fill: "#000",
+    "font-size": 4,
 
+  });
+  for (let i = 0; i < o.members.length; ++i) {
+    let text = appendSVG(texts, "text", { x: 1, y: i * 10 + 7 });
+    text.innerHTML = o.members[i].name;
+  }
+};
 const showFiles = (files) => {
   elSVG.innerHTML = '';
-  if (1 < files.length) {
+  if (1 != files.length) {
     alert("Not a single file was drpoped.");
     return;
   }
   let file = files[0];
+  console.log(files);
   if ("application/json" != file.type) {
     alert(`dropped file ${file.name} is not a json`);
     return;
@@ -79,8 +142,9 @@ const showFiles = (files) => {
 };
 
 bulidSVG({
-  width: "500",
-  height: "500",
+  width: 800,
+  height: 400,
+  graph_left: 100,
   unit: "px",
   members: [
     {
